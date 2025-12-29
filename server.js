1fc923dd9324ae9a21e15748bcf5101aff86dc8b
Emstrong Lezin <emstronglezin@gmail.com>
2025-12-29 16:12:15 +0000

    ecoute du backend

--- server.js
+++ server.js
@@ -88,6 +88,6 @@ io.on('connection', (socket) => {
 });
 
 // Start server
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
